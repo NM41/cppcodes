37aaee54ba5a383ebde64853f4a2292b87ca42ab
Neon2190 <neon.parmar.001@gmail.com>
2023-07-06 00:45:31 +0530

    create a first cpp file

---
+++ src/first.cpp
@@ -0,0 +1,14 @@
+//Desc : first cpp hello world program
+//Date : 05 jul 2023
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"----------------------------------------------------------"<<endl;
+
+    cout<<"Hello World";
+
+    cout<<endl<<"----------------------------------------------------------"<<endl;
+    return 0;
+}   
